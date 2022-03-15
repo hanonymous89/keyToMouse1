6891dfe74c88469d5819f57df8130fbd7c496a90
hanonymous89 <96468681+hanonymous89@users.noreply.github.com>
2022-03-15 19:59:04 +0900

    Add files via upload

--- keyToMouse2.cpp
+++ keyToMouse2.cpp
@@ -18,7 +18,7 @@ namespace ano {
 		return data;
 	}
 	auto stoull(std::string str,unsigned long long def) {
-		if (!str.empty()&&std::all_of(str.cbegin(), str.cend(), [](char c) {return std::isdigit(c); })) {//all_of(,,isdigit)error!
+		if (!str.empty()&&std::all_of(str.cbegin(), str.cend(), [](char c) {return std::isdigit(c); })) {
 			return std::stoull(str);
 		}
 		return def;
@@ -38,6 +38,17 @@ namespace ano {
 		auto pos = getCursorPos();
 		SetCursorPos(pos.x + x, pos.y + y);
 		return pos;
+	}
+	inline auto mouseEvent(DWORD flag, DWORD data) {
+		auto pos = ano::getCursorPos();
+		INPUT input;
+		input.type = INPUT_MOUSE;
+		input.mi.dx = pos.x;
+		input.mi.dy = pos.y;
+		input.mi.time = input.mi.dwExtraInfo = 0;
+		input.mi.dwFlags = flag;
+		input.mi.mouseData = data;
+		return  SendInput(1, &input, sizeof(input));
 	}
 	class File {
 	private:
@@ -63,11 +74,12 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR lpCmdLine,in
 	};
 	constexpr auto DEFAULT_BOOST = 1,DEFAULT_SPEED=0;
 	const auto data=ano::split(ano::File("data.txt").read().getContent(),"\n");
-	const auto interval = DATA::SPEED < data.size() ? ano::stoull(data[DATA::SPEED], DEFAULT_SPEED) : DEFAULT_SPEED;
+	const auto interval = DATA::SPEED < data.size() ? ano::stoull(data[DATA::SPEED], DEFAULT_SPEED) : DEFAULT_SPEED;//boost=default
 	const decltype(interval) boost = (interval / (DATA::BOOST < data.size() ? ano::stod(data[DATA::BOOST], DEFAULT_BOOST) : DEFAULT_BOOST));
-	while (!GetAsyncKeyState(VK_SPACE)) {
+	bool lClk=false, rClk=false, mClk=false;
+	while (!GetAsyncKeyState('Q')) {//multiThread
 		constexpr auto SPEED = 1;
-		std::this_thread::sleep_for(std::chrono::nanoseconds((GetAsyncKeyState(VK_SHIFT)!=0)?boost:interval));//interval/is_boost?boost:defboost
+		std::this_thread::sleep_for(std::chrono::nanoseconds((GetAsyncKeyState(/*VK_SPACE*/VK_SHIFT) != 0) ? boost : interval));//interval/is_boost?boost:defboost
 		int xSpeed=0, ySpeed= 0;
 		if (GetAsyncKeyState(0x4A)!=0) {
 			xSpeed = -SPEED;
@@ -83,6 +95,36 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR lpCmdLine,in
 		}
 		if (xSpeed || ySpeed) {
 			ano::setCursorRelativePos(xSpeed, ySpeed);
+		}
+		if (GetAsyncKeyState('G')!=0) {
+			ano::mouseEvent(MOUSEEVENTF_HWHEEL,-WHEEL_DELTA);
+		}
+		else if (GetAsyncKeyState('H') != 0) {
+			ano::mouseEvent(MOUSEEVENTF_HWHEEL,WHEEL_DELTA);
+		}
+		if (!lClk && GetAsyncKeyState('S')) {
+			lClk = true;
+			ano::mouseEvent(MOUSEEVENTF_LEFTDOWN, 0);
+		}
+		else if (lClk && !GetAsyncKeyState('S')) {
+			lClk = false;
+			ano::mouseEvent(MOUSEEVENTF_LEFTUP, 0);
+		}
+		if (!rClk && GetAsyncKeyState('F')) {
+			rClk = true;
+			ano::mouseEvent(MOUSEEVENTF_RIGHTDOWN, 0);
+		}
+		else if (rClk && !GetAsyncKeyState('F')) {
+			rClk = false;
+			ano::mouseEvent(MOUSEEVENTF_RIGHTUP, 0);
+		}
+		if (!mClk && GetAsyncKeyState('D')) {
+			mClk = true;
+			ano::mouseEvent(MOUSEEVENTF_MIDDLEDOWN, 0);
+		}
+		else if (mClk && !GetAsyncKeyState('D')) {
+			mClk = false;
+			ano::mouseEvent(MOUSEEVENTF_MIDDLEUP, 0);
 		}
 	}
 	return 0;
